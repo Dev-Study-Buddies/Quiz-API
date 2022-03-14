425b889df136e02965b38de4965370a77ca5c41e
riboney <14286113+Ribby-0254@users.noreply.github.com>
2022-03-14 06:58:59 -0400

    Renames API to HTTP

---
+++ src/http/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const timeout = require('connect-timeout')
+require('express-async-errors')
+
+const routes = require('./app.router')
+const e = require('./middlewares/error.middleware')
+const m = require('./middlewares/misc.middleware')
+
+const app = express()
+
+app.use(timeout(3000))
+app.use(express.json())
+app.use(cors())
+app.use(m.httpTimeout)
+
+app.use(routes)
+
+app.use(e.errorLogger)
+app.use(e.errorHandler)
+
+module.exports = app
